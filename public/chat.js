965b5441e33846752471d790e100ce87a64bc125
Aishwarya Subramanian <aishwarya@Aishwaryas-MacBook-Air.local>
2015-04-20 18:02:59 -0400

    Sending message when enter id pressed.

--- public/chat.js
+++ public/chat.js
@@ -1,15 +1,27 @@
-window.onload = function(){
+$(document).ready(function(){
+
 	var socket = io();
 
 	$('#send').click(function(){
-		user_msg = $('#user-msg').val();
-		user_name = $('#user-name').val();
-		socket.emit('send', {message: user_msg, name: user_name});
-		$('#user-msg').val('');
+		sendMessage();
+	});
+
+	$('#user-msg').keyup(function(e){
+		if(e.keyCode == 13){
+			sendMessage();
+		}
 	});
 
 	socket.on('message', function(data){
 		sender = data.name ? data.name : 'Server'
 		$('#content').append('<b>' + sender + ': ' + '</b>' + data.message + '<br/>')
 	})
+});
+
+function sendMessage(){
+	var socket = io();
+	user_msg = $('#user-msg').val();
+	user_name = $('#user-name').val();
+	socket.emit('send', {message: user_msg, name: user_name});
+	$('#user-msg').val('');
 }
