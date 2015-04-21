75e5c485499d426d1c0a75c81c7771a88c0664b2
Aishwarya Subramanian <aishwarya@Aishwaryas-MacBook-Air.local>
2015-04-21 17:38:31 -0400

    Moved contact info to another page.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = '3000';
+var port = process.env.PORT || '3000';
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', 'jade');
@@ -11,20 +11,26 @@ app.get('/chat', function(req, res){
 });
 
 app.use(express.static(__dirname + '/public'));
-var io = require('socket.io').listen(app.listen(process.env.PORT || 3000, function(){
+var io = require('socket.io').listen(app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 }));
 
 io.sockets.on('connection', function(socket){
-	socket.emit('welcome', {message: 'welcome to chat'});
+
+	socket.on('enter room', function(name){
+		socket.username = name
+		console.log('User ' + name + ' has entered.')
+		io.sockets.emit('user entered room', name)
+	});
+
 	socket.on('send', function(data){
 		console.log('Message received from ' + data.name)
 		io.sockets.emit('display message', data)
 	})
 
 	socket.on('start typing', function(data){
-		console.log('User ' + data.name + ' has started typing')
-		io.sockets.emit('user typing started', data)
+		console.log('User ' + socket.username + ' has started typing')
+		io.sockets.emit('user typing started', socket.username)
 	})
 
 	socket.on('stop typing', function(data){
--- public/chat.js
+++ public/chat.js
@@ -1,8 +1,26 @@
 var socket;
 
 $(document).ready(function(){
-
+	$('#chat-div').hide();
 	socket = io();
+
+	$('#contact-submit').click(function(){
+		socket.emit('enter room', $('#user-name').val());
+		$('#user-detail-div').hide();
+		$('#chat-div').show();
+	});
+
+	socket.on('user entered room', function(name){
+		$('#content').append('<p>' + name + ' entered room.</p>')
+	});
+
+	$('#user-msg').focusin(function(e){
+		typingStarted();
+	});
+
+	$('#user-msg').focusout(function(e){
+		typingStopped();
+	});
 
 	$('#send').click(function(){
 		sendMessage();
@@ -14,34 +32,25 @@ $(document).ready(function(){
 		}
 	});
 
-	$('#user-msg').focusin(function(e){
-		typingStarted();
-	});
-
-	$('#user-msg').focusout(function(e){
-		typingStopped();
-	});
-
 	socket.on('display message', function(data){
 		sender = data.name ? data.name : 'Server'
 		$('#content').append('<b>' + sender + ': ' + '</b>' + data.message + '<br/>')
-	})
+	});
 
-	socket.on('user typing started', function(data){
-		if ( !$( "#" + data.name ).length ) {
-			$('#content').append('<p id=' + data.name + '>' + data.name + ' is typing..</p>')
+	socket.on('user typing started', function(name){
+		if ( !$( "#" + name ).length ) {
+			$('#content').append('<p id=' + name + '>' + name + ' is typing..</p>')
 		}
-	})
+	});
 
 	socket.on('user typing stopped', function(data){
 		$( "p" ).remove( "#" + data.name );
-	})
+	});
 
 });
 
 function typingStarted(){
-	var user_name = $('#user-name').val();
-	socket.emit('start typing', {name: user_name});
+	socket.emit('start typing', {});
 }
 
 function typingStopped(){
@@ -50,7 +59,6 @@ function typingStopped(){
 }
 
 function sendMessage(){
-	$( "p" ).remove( "#type-msg" );
 	user_msg = $('#user-msg').val();
 	typingStopped();
 	socket.emit('send', {message: user_msg, name: user_name});
